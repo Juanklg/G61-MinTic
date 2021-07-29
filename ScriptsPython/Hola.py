9edac0c20790f719d8f6da56e42f00167ba5e860
DESKTOP-QI3AGC3\Juan <juanklg25@gmail.com>
2021-07-29 15:05:15 -0500

    Add Script Hola.py, Add Jupyter files

---
+++ ScriptsPython/Hola.py
@@ -0,0 +1,16 @@
+print("Aca empieza mi programa -----------------------------------------------------------------")
+#asi se crea una variable
+miVar = 10
+#asi se guarda en una var la pocion de memoria de una variable
+pos = id(miVar)
+#asi python imprime en consola una variable
+print(miVar)
+print(pos)
+#asi se crea una variable de texto
+miVar2 = "Grupo 61"
+print(miVar2)
+#asi python imprime cualquier texto
+print("Cualquier texto")
+#asi se imprime un texto concatenado
+print("miVar2 es igtual a  : "+miVar2)
+print("Aca termina mi programa-----------------------------------------------------------------")
