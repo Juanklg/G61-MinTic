db3f23a4863d84999f35ec919a15cbd46f1352ee
DESKTOP-QI3AGC3\Juan <juanklg25@gmail.com>
2021-08-05 16:07:24 -0500

    Add turtle, vida, update registro

--- Scripts/Actividades/7-Registro.py
+++ Scripts/Actividades/7-Registro.py
@@ -1,4 +1,4 @@
-print(f"Para el registro se solictan los siguientes datos")
+print("Para el registro se solictan los siguientes datos")
 nombre = input("Ingrese nombre :")
 edad = input("Ingrese edad (debe ser en numeros) : ")
 altura = input("Ingrese altura (en numeros) : ")
@@ -7,21 +7,18 @@ rolAdmin = input("Ingrese rol Administrador True/False : ")
 try:    
     edad = int(edad)
     altura = float(altura)
-
     #convirtiendo nuestro ingreso en la variable rolAdmin a Booleano
     if rolAdmin=="True" or rolAdmin=="False":
         if rolAdmin == "True" : rolAdmin=True
         elif rolAdmin == "False" : rolAdmin=False
     else:
-        #Si elo usuario no digito True o False se cierra la app y se indica q la inicie de nuevo
-        print("El valor ingresado en el rol Administrador debe ser False o True")
-        print("vuelva a correr la aplicacion")
+        #como no se cumplio la condicionforzo el erro y lanza la except
         exit()
     
 #si no lanza ningun error continua el codigo normal
 #si lanza algun error retorna el bloque de codigo despues del except
 except:
-    print("Alguno de los valore ingresados no es correcto")
+    print("Alguno de los valores ingresados no es correcto")
     print("vuelva a correr la aplicacion")
     #le damos fin a la aplicacion por error del usuario
     exit()
@@ -34,12 +31,11 @@ print(f''' Registro Exitoso
 ''')
 
 if rolAdmin==True:
-    print("El usuario no puede ser administrador entonces cambieremos su rol")
+    print("El usuario no puede ser administrador entonces actualizamos su rol")
     rolAdmin = not rolAdmin
-
-print(f''' Registro Exitoso
-    Nombre = {nombre}
-    Edad = {edad}
-    Altura = {altura} metros
-    Administrador = {rolAdmin}
-''')
+    print(f''' Registro actualizado
+        Nombre = {nombre}
+        Edad = {edad}
+        Altura = {altura} metros
+        Administrador = {rolAdmin}
+    ''')
