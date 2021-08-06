a06fb2a8e822b18bf43b903ba766588d5bf1f72a
DESKTOP-QI3AGC3\Juan <juanklg25@gmail.com>
2021-08-06 15:15:20 -0500

    add hexagono

---
+++ Scripts/1-HexagonoAzul.py
@@ -0,0 +1,34 @@
+import turtle
+
+t = turtle.Turtle()
+
+# primeras lineas
+# t.forward(100)
+# t.left(45)
+# t.forward(100)
+
+#cambieando el color del trazo
+t.color("blue")
+
+# poligono de 6 lados
+t.begin_fill()
+t.forward(100)
+t.left(45)
+t.forward(100)
+t.left(45)
+t.forward(100)
+t.left(45)
+t.forward(100)
+t.left(45)
+t.forward(100)
+t.left(45)
+t.forward(100)
+t.left(45)
+t.forward(100)
+t.left(45)
+t.forward(100)
+t.end_fill()
+
+
+
+turtle.done()
