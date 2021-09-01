5e46b13d466e2eb0ffdc990fada9ac63685ffb74
Juan Linares <juanklg25@gmail.com>
2021-09-01 16:04:22 -0500

    update html to proy

--- dev.py
+++ dev.py
@@ -1,7 +1,11 @@
 from funciones import isLogin
 
 #--------------------------------------------------
-
+def solicitarDoc():
+    print('Se esta lanzando este evento'.center(50,'-'))
+    suma = 1+1
+    print(f'resultado de la suma {suma}'.center(50,'-'))
+    return suma
 
 
 
--- index.py
+++ index.py
@@ -1,28 +1,30 @@
 from flask import Flask , render_template
 import dev as fn
 # import funciones as fn
-
 fn.isLogin('Login')
 print('Corriendo la app',__name__)
 # Inicializacion ------------------------------------------------
-
 #Configura la app
 app = Flask(__name__)
-
 # Crea las diferentes rutas -------------------------------------
 @app.route("/")
 def hello_world():    
-    return "Hola Grupo 61"
-
-@app.route("/home")
-def index():
     return render_template("index.html")
-
+    
 @app.route("/proy")
 def proy():
+    s = fn.solicitarDoc()
+    print(s)    
     return render_template("index.html")
-# Cierre de rutas ----------------------------------------------    
-
+    
+@app.route("/doc")
+def doc():
+    return render_template("doc/index.html")
+@app.route("/doc/<documento>")
+def docu(documento='index'):
+    print(f'var = {documento}'.center(50,'-'))
+    return render_template(f"doc/{documento}.html")
+# Cierre de rutas ---------------------------------------------- 
 # Aca inicia la aplicacion si es el archivo principal
 if __name__ == "__main__":
     print("Main".center(50,'-'))
