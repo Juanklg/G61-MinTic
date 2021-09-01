a70d428588fca6ee3af2ea14ccdb9c20f9f761b5
Juan Linares <juanklg25@gmail.com>
2021-09-01 14:36:25 -0500

    organizando arquitectura

--- funciones.py
+++ funciones.py
@@ -1,13 +1,28 @@
-from datetime import datetime
-import crud as crud
-# import crudXls as crud
-
 #Este es el archivo principal de nuestro proyecto
 
 #Variables {int,float,str,bool}
 #iterables o colecciones = {dict,list,tuple,set,range}
 #funciones {propias,python,class}
 #bucles o sentencias de control = {if,while,for}
+from datetime import datetime
+# import crud as crud
+# import crudXls as crud
+
+
+
+# ----------------------------Organizadas--------------------------------------------
+def isLogin(user)->bool:
+    login = None
+    if user=="Login":
+        login = True
+        print("Usuario esta logueado")
+    else:
+        login = False
+        print("Usuario no esta logueado, Chao")
+        exit()
+    return login
+
+# ----------------------------Por organizar--------------------------------------------
 
 def menuFinal(rut):
     #lista de parametros para creacion
@@ -226,18 +241,62 @@ def printMenu(el):
     for i,e in zip(range(1,len(el)+1),el):
         print(f"\t{i} - {e}")
 
-def isLogin(user)->bool:
-    login = None
-    if user=="Login":
-        login = True
-        print("Usuario esta logueado")
-    else:
-        login = False
-        print("Usuario no esta logueado, Chao")
-        exit()
-    return login
-
 if __name__ == "__main__":
     rut = r"C:\Users\MakeDream\Desktop\Ruta1\G61-MinTic\dbTask.xlsx"    
     menuOriginal(rut)
 else: print("Corriendo como secundario",__name__)
+
+
+# #creamos un diccionario para estudiantes desde la lista de estudiantes
+
+# dictEstudiantes = dict.fromkeys(listEstudiantes)
+# #creamos un diccionario desde la tupla con las caracteristicas de un estudiante
+# dictInfoEstudiante = dict.fromkeys(tuplaEstudiantes)
+
+# #creo un copia de mi diccioario de estudiantes y recorro el diccinario de estudiantes y les asigno asl propiedas de los estudiantes a todos
+# dictEstudiantesCopy = dictEstudiantes.copy()
+# for estudiante in dictEstudiantesCopy.keys():
+#     dictEstudiantes[estudiante]=dictInfoEstudiante
+# printDict(dictEstudiantes)    
+
+# for estudiante in dictEstudiantesCopy.keys():
+#     print(estudiante)
+#     dictEstudiantes[estudiante]['nombre']=str(estudiante)
+#     dictEstudiantes[estudiante]['edad']=15
+#     dictEstudiantes[estudiante]['grupo']=61
+#     dictEstudiantes[estudiante]['apellido']=''
+#     dictEstudiantes[estudiante]['nota']=0.0
+
+# Estudiantes = [
+#     {'nombre': 'Andres', 'edad': 15, 'grupo': 61, 'apellido': '', 'nota': 0.0},
+#     {'nombre': 'Brayan', 'edad': 15, 'grupo': 61, 'apellido': '', 'nota': 0.0},
+#     {'nombre': 'Jhon', 'edad': 15, 'grupo': 61, 'apellido': '', 'nota': 0.0},
+#     {'nombre': 'Juan', 'edad': 15, 'grupo': 61, 'apellido': '', 'nota': 0.0},
+#     {'nombre': 'Karen', 'edad': 15, 'grupo': 61, 'apellido': '', 'nota': 0.0},
+#     {'nombre': 'Maria', 'edad': 15, 'grupo': 61, 'apellido': '', 'nota': 0.0},
+#     {'nombre': 'Nicolas', 'edad': 15, 'grupo': 61, 'apellido': '', 'nota': 0.0},
+#     {'nombre': 'Stuart', 'edad': 15, 'grupo': 61, 'apellido': '', 'nota': 0.0},
+#     {'nombre': 'Valentina', 'edad': 15, 'grupo': 61, 'apellido': '', 'nota': 0.0}
+# ]
+
+# dictEstudiants = {
+#     "Andres":{'nombre': 'Andres', 'edad': 15, 'grupo': 61, 'apellido': '', 'nota': 0.0},
+#     "Brayan":{'nombre': 'Brayan', 'edad': 15, 'grupo': 61, 'apellido': '', 'nota': 0.0},
+#     "Jhon":{'nombre': 'Jhon', 'edad': 15, 'grupo': 61, 'apellido': '', 'nota': 0.0},
+#     "Juan":{'nombre': 'Juan', 'edad': 15, 'grupo': 61, 'apellido': '', 'nota': 0.0},
+#     "Karen":{'nombre': 'Karen', 'edad': 15, 'grupo': 61, 'apellido': '', 'nota': 0.0},
+#     "Maria":{'nombre': 'Maria', 'edad': 15, 'grupo': 61, 'apellido': '', 'nota': 0.0},
+#     "Nicolas":{'nombre': 'Nicolas', 'edad': 15, 'grupo': 61, 'apellido': '', 'nota': 0.0},
+#     "Stuart":{'nombre': 'Stuart', 'edad': 15, 'grupo': 61, 'apellido': '', 'nota': 0.0},
+#     "Valentina":{'nombre': 'Valentina', 'edad': 15, 'grupo': 61, 'apellido': '', 'nota': 0.0}
+# }
+
+# pr='nombre'
+# if Estudiantes['Juan'][pr] == Estudiantes['Maria'][pr]:
+#     print('La propiedad es igual')
+# else:
+#     print('La propiedad es diferente')
+
+# # set
+# tipoSet = {}
+
--- index.py
+++ index.py
@@ -1,9 +1,11 @@
-from numpy.core.records import array
-import funciones as fn
 from flask import Flask , render_template
+import dev as fn
+# import funciones as fn
 
 fn.isLogin('Login')
 print('Corriendo la app',__name__)
+# Inicializacion ------------------------------------------------
+
 #Configura la app
 app = Flask(__name__)
 
@@ -15,77 +17,15 @@ def hello_world():
 @app.route("/home")
 def index():
     return render_template("index.html")
-# Crea las diferentes rutas -------------------------------------    
+
+@app.route("/proy")
+def proy():
+    return render_template("index.html")
+# Cierre de rutas ----------------------------------------------    
 
 # Aca inicia la aplicacion si es el archivo principal
 if __name__ == "__main__":
     print("Main".center(50,'-'))
-    # ventas = pd.Series([15,12,21],index=["Ene","Feb","Mar"])
-    # print(ventas)
-    
-    
-    
-
-
-
-
-
-
-    
-    #debug=True me permite trabajar en desarrollo y actualizar el servidor cada vez que se hagan cambios
-    # app.run(debug=True)
-
-
-
-# #creamos un diccionario para estudiantes desde la lista de estudiantes
-
-# dictEstudiantes = dict.fromkeys(listEstudiantes)
-# #creamos un diccionario desde la tupla con las caracteristicas de un estudiante
-# dictInfoEstudiante = dict.fromkeys(tuplaEstudiantes)
-
-# #creo un copia de mi diccioario de estudiantes y recorro el diccinario de estudiantes y les asigno asl propiedas de los estudiantes a todos
-# dictEstudiantesCopy = dictEstudiantes.copy()
-# for estudiante in dictEstudiantesCopy.keys():
-#     dictEstudiantes[estudiante]=dictInfoEstudiante
-# printDict(dictEstudiantes)    
-
-# for estudiante in dictEstudiantesCopy.keys():
-#     print(estudiante)
-#     dictEstudiantes[estudiante]['nombre']=str(estudiante)
-#     dictEstudiantes[estudiante]['edad']=15
-#     dictEstudiantes[estudiante]['grupo']=61
-#     dictEstudiantes[estudiante]['apellido']=''
-#     dictEstudiantes[estudiante]['nota']=0.0
-
-# Estudiantes = [
-#     {'nombre': 'Andres', 'edad': 15, 'grupo': 61, 'apellido': '', 'nota': 0.0},
-#     {'nombre': 'Brayan', 'edad': 15, 'grupo': 61, 'apellido': '', 'nota': 0.0},
-#     {'nombre': 'Jhon', 'edad': 15, 'grupo': 61, 'apellido': '', 'nota': 0.0},
-#     {'nombre': 'Juan', 'edad': 15, 'grupo': 61, 'apellido': '', 'nota': 0.0},
-#     {'nombre': 'Karen', 'edad': 15, 'grupo': 61, 'apellido': '', 'nota': 0.0},
-#     {'nombre': 'Maria', 'edad': 15, 'grupo': 61, 'apellido': '', 'nota': 0.0},
-#     {'nombre': 'Nicolas', 'edad': 15, 'grupo': 61, 'apellido': '', 'nota': 0.0},
-#     {'nombre': 'Stuart', 'edad': 15, 'grupo': 61, 'apellido': '', 'nota': 0.0},
-#     {'nombre': 'Valentina', 'edad': 15, 'grupo': 61, 'apellido': '', 'nota': 0.0}
-# ]
-
-# dictEstudiants = {
-#     "Andres":{'nombre': 'Andres', 'edad': 15, 'grupo': 61, 'apellido': '', 'nota': 0.0},
-#     "Brayan":{'nombre': 'Brayan', 'edad': 15, 'grupo': 61, 'apellido': '', 'nota': 0.0},
-#     "Jhon":{'nombre': 'Jhon', 'edad': 15, 'grupo': 61, 'apellido': '', 'nota': 0.0},
-#     "Juan":{'nombre': 'Juan', 'edad': 15, 'grupo': 61, 'apellido': '', 'nota': 0.0},
-#     "Karen":{'nombre': 'Karen', 'edad': 15, 'grupo': 61, 'apellido': '', 'nota': 0.0},
-#     "Maria":{'nombre': 'Maria', 'edad': 15, 'grupo': 61, 'apellido': '', 'nota': 0.0},
-#     "Nicolas":{'nombre': 'Nicolas', 'edad': 15, 'grupo': 61, 'apellido': '', 'nota': 0.0},
-#     "Stuart":{'nombre': 'Stuart', 'edad': 15, 'grupo': 61, 'apellido': '', 'nota': 0.0},
-#     "Valentina":{'nombre': 'Valentina', 'edad': 15, 'grupo': 61, 'apellido': '', 'nota': 0.0}
-# }
-
-# pr='nombre'
-# if Estudiantes['Juan'][pr] == Estudiantes['Maria'][pr]:
-#     print('La propiedad es igual')
-# else:
-#     print('La propiedad es diferente')
-
-# # set
-# tipoSet = {}
+    #debug=True me permite trabajar en desarrollo 
+    #y actualizar el servidor cada vez que se hagan cambios
+    app.run(debug=True)
